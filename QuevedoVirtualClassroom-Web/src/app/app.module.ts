1695e5eef1c03c73e0a983ee0b701c35bea2715a
Padilla Delgado <andre.padilla@realtech.es>
2022-04-07 11:46:09 +0200

    Inicio Angular

--- QuevedoVirtualClassroom-Web/src/app/app.module.ts
+++ QuevedoVirtualClassroom-Web/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UploadComponent } from './pages/upload/upload.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DragDropComponent } from './components/drag-drop/drag-drop.component';
+import { HttpClientModule } from '@angular/common/http'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideosComponent,
+    LoginComponent,
+    NavbarComponent,
+    UploadComponent,
+    DragDropComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
